042595d17c9283570399708ff8a556584d45252f
CAUÃ NUNES DE LIMA SALES <caua.sales@a.ucb.br>
2024-03-13 09:44:39 -0300

    Atividade1

---
+++ Atividade1/variaveisOperadores.js
@@ -0,0 +1,20 @@
+let x = 2
+let y = 4
+let soma = x + y
+ 
+let o = 6
+let p = 12
+let sub = o - p
+
+let q = 10
+let f = 8
+let mult= q * f
+
+let u = 50
+let b = 5
+let div = u / b
+
+console.log('Resultado da soma entre ' + x + ' e ' + y + ' : ' + soma)
+console.log('Resultado da subtração entre ' + o + ' e ' + p + ' : ' + sub)
+console.log('Resultado da multiplicação entre ' + q + ' e ' + f + ' : ' + mult)
+console.log('Resultado da divisão entre ' + u + ' e ' + b + ' : ' + div)
