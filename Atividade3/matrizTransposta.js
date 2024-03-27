541058cf9b8c7fc40659ea32338705d38ffb1c24
CAUÃ NUNES DE LIMA SALES <caua.sales@a.ucb.br>
2024-03-27 10:07:03 -0300

    Atualizando codigo

--- Atividade3/matrizTransposta.js
+++ Atividade3/matrizTransposta.js
@@ -1,12 +1,22 @@
 function  transporMatriz(A){
 let matrizString = "";
-for(let i = 0; i < A.lenght; i++){
-    for(let j = 0; j < A[i].lenght; j++){
-        matrizString += A[i][j] + "";
+for(let i = 0; i < A.length; i++){
+    for(let j = 0; j < A[i].length; j++){
+        matrizString += A[i][j] + " ";
     }
     matrizString += '\n';
 }
 console.log(matrizString);
+
+let matrizTransportaString = "";
+for(let j = 0; j <A[0].length; j++){
+    for(let i = 0; i < A.length; i++){
+        matrizTransportaString += A[i][j] + " ";
+    }
+    matrizTransportaString += "\n";
+}
+
+console.log(matrizTransportaString);
 
 }
 
