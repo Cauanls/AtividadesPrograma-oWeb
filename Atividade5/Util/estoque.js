fc3afaf9ef3870a81f4ae3b0832aefcfa49ef99a
CauaNunes <cauanunes.200406@gmail.com>
2024-04-17 12:51:43 -0300

    Tentei fazer a funcao editar mas deu erro

--- Atividade5/Util/estoque.js
+++ Atividade5/Util/estoque.js
@@ -1,4 +1,6 @@
-function criarProduto(id, nome, qtd){
+var produtos = []
+
+function criarProduto(id, nome, qtd) {
     let p = {
         id: id,
         nome: nome,
@@ -7,31 +9,34 @@ function criarProduto(id, nome, qtd){
     return p;
 }
 
-function adcionarProduto(p){
+function adcionarProduto(p) {
     produtos.push(p);
 }
 
-function listarProdutos(){
+function listarProdutos() {
     return produtos;
 }
 
-function removerProduto(id){
+function removerProduto(id) {
     produtos = produtos.filter((p) => {
         return p.id != id
     });
 }
 
-function editarProduto(id, qtd){
-    produtos = produtos.filter((id) => {
-        atualizar = qtd;
-        return p.qtd = atualizar
-    })
+function editarProduto(id, qtd, p) {
+    const produto = p.find(produto => produto.id === id);
+    if (produto) {
+        p.qtd = parseInt(qtd);
+        res.send('Quantidade do produto atualizada.');
+    } else {
+        res.send('Produto não encontrado.');
+    }
 }
 
 module.exports = {
     criarProduto,
     adcionarProduto,
     listarProdutos,
-    removerProduto, 
+    removerProduto,
     editarProduto
 }
--- Atividade5/index.js
+++ Atividade5/index.js
@@ -1,18 +1,17 @@
 const express = require('express');
 const app = express();
 const estoque = require('./Util/estoque')
-
-
 const PORT = 8080;
-app.listen(PORT, function() {
+
+app.listen(PORT, function () {
     console.log("app rodando na porta" + PORT);
 })
 
-app.get('/', function(req, res){
+app.get('/', function (req, res) {
     res.send("Tudo certo!")
 })
 
-app.get('/adcionar/:id/:nome/:qtd', function(req, res){
+app.get('/adicionar/:id/:nome/:qtd', function (req, res) {
     let id = req.params.id;
     let nome = req.params.nome;
     let qtd = req.params.qtd;
@@ -21,19 +20,20 @@ app.get('/adcionar/:id/:nome/:qtd', function(req, res){
     res.send(p)
 })
 
-app.get('/listar', function(req, res){
+app.get('/listar', function (req, res) {
     res.send(estoque.listarProdutos());
 })
 
-app.get('/remover/:id', function(req, res){
+app.get('/remover/:id', function (req, res) {
     let id = req.params.id;
     estoque.removerProduto(id);
     res.send('removido o produto com id: ' + id)
 })
 
-app.get("/editar/:id/:qtd", function(req, res){
+app.get("/editar/:id/:qtd", function (req, res) {
     let id = req.params.id;
     let qtd = req.params.qtd;
-    estoque.editarProduto(id, qtd);
-    res.send('quantidade do' + id + 'atualizada!')  
+    let p = req.params.qtd;
+    estoque.editarProduto(id, qtd, p);
+    res.send('quantidade do ' + id + ' atualizada!')
 })
